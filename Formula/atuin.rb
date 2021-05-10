3c72797d79e75d2b16396b421d775d38e79f8b67
Ellie Huxtable <e@elm.sh>
2021-05-10 22:58:11 +0100

    Update to 0.7.1

--- Formula/atuin.rb
+++ Formula/atuin.rb
@@ -1,9 +1,9 @@
 class Atuin < Formula
   desc "Magical shell history"
   homepage "https://github.com/ellie/atuin"
-  url "https://github.com/ellie/atuin/releases/download/v0.6.4/atuin-v0.6.4-x86_64-apple-darwin.tar.gz"
-  sha256 "45ee6b440c4d2e1cbeb90a04ed7e09cbc82d3483d0319fc977b23c9ff66c3481"
-  version "0.6.4"
+  url "https://github.com/ellie/atuin/releases/download/v0.7.1/atuin-v0.7.1-x86_64-apple-darwin.tar.gz"
+  sha256 "f651499279126477b5658d42b884349e2148daa62270de87894192520b2ab3ae"
+  version "0.7.1"
 
   def install
     bin.install "atuin"
